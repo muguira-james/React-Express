cf6fc40ae1b4d089e5949af7e66cf4d0e04198f2
muguira <muguira@localhost.localdomain>
2019-09-28 19:11:04 -0400

    formatting

--- bend/tests/api.js
+++ bend/tests/api.js
@@ -3,7 +3,8 @@ import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/all'
 export const getAllPlayers = async () => {
-    const json = await axios.get(URL).then( resp => { return resp.data })
+    const json = await axios.get(URL)
+                        .then( resp => { return resp.data })
     // console.log("--->", json) 
     return json
 }
